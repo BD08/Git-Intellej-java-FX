c89b92f06903e332556c4810846d781eec3f606c
bang_dolla08 <bangdolla08@gmail.com>
2018-10-30 09:45:55 +0700

    Siap Dapper

--- src/fxtebaexpressnb/DatabaseManajement/TableEntity/RelationViewTransactionList/PengirimanTransactionModel.java
+++ src/fxtebaexpressnb/DatabaseManajement/TableEntity/RelationViewTransactionList/PengirimanTransactionModel.java
@@ -1,9 +1,13 @@
 package fxtebaexpressnb.DatabaseManajement.TableEntity.RelationViewTransactionList;
 
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import fxtebaexpressnb.DatabaseManajement.TableEntity.TableTarif;
 import fxtebaexpressnb.DatabaseManajement.TypeCheckPoint;
 import fxtebaexpressnb.DatabaseManajement.TypeCheckPointClass;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 
-public class PengirimanTransactionModel {
+public class PengirimanTransactionModel extends RecursiveTreeObject<TableTarif> {
 
 	public static String IDTRANSACTION_COLOUMN="idTransaction";
 	public static String NAMAPENGIRIM_COLOUMN="namaPengirim";
@@ -16,95 +20,140 @@ public class PengirimanTransactionModel {
 	public static String TANGGALCHECKIN_COLOUMN="CreateDate";
 	public static String CHECKPOINTNAME_CLOUMN="FirstName";
 
-	private int idTransaction;
-	private String namaPengirim;
-	private String namaPenerima;
-	private String alamatPengirim;
-	private String alamatPenerima;
-	private int idCheckPoint;
-	private int typeCheckPointInt;
-	private TypeCheckPoint typeCheckPoint;
-	private String tanggalKirim;
-	private String tanggalCheckIn;
+	public int idTransaction;
+	public String namaPengirim;
+	public String namaPenerima;
+	public String alamatPengirim;
+	public String alamatPenerima;
+	public int idCheckPoint;
+	public int typeCheckPointInt;
+	public TypeCheckPoint typeCheckPoint;
+	public String tanggalKirim;
+	public String tanggalCheckIn;
 
-	private String namaPembawa;
+	public String namaPembawa;
+	public String NOAWB;
 
-	public int getIdTransaction() {
-		return idTransaction;
+	public SimpleIntegerProperty getSimpleStringIdTransaction(){
+		return new SimpleIntegerProperty(idTransaction);
 	}
 
-	public void setIdTransaction(int idTransaction) {
-		this.idTransaction = idTransaction;
+	public SimpleStringProperty getSimpleStringPropertyNamaPengirim(){
+		return new SimpleStringProperty(namaPengirim);
 	}
 
-	public String getNamaPengirim() {
-		return namaPengirim;
+	public SimpleStringProperty getSimpleStringPropertyNamaPenerima(){
+		return new SimpleStringProperty(namaPenerima);
 	}
 
-	public void setNamaPengirim(String namaPengirim) {
-		this.namaPengirim = namaPengirim;
+	public SimpleStringProperty getSimpleStringPropertyAlamatPengirim(){
+		return new SimpleStringProperty(alamatPengirim);
 	}
 
-
-	public String getNamaPenerima() {
-		return namaPenerima;
+	public SimpleStringProperty getSimpleStringPropertyAlamatPenerima(){
+		return new SimpleStringProperty(alamatPenerima);
 	}
 
-	public void setNamaPenerima(String namaPenerima) {
-		this.namaPenerima = namaPenerima;
+	public SimpleIntegerProperty getSimpleIntegerPropertyIdCheckPoint(){
+		return new SimpleIntegerProperty(idCheckPoint);
 	}
 
-	public String getAlamatPengirim() {
-		return alamatPengirim;
+	public SimpleStringProperty getSimpleStringPropertyTypeCheckPoint(){
+		return new SimpleStringProperty(TypeCheckPointClass.getCheckPointType(typeCheckPointInt).toString());
 	}
 
-	public void setAlamatPengirim(String alamatPengirim) {
-		this.alamatPengirim = alamatPengirim;
+	public  SimpleStringProperty getSimpleStringPropertyTanggalKirim(){
+		return new SimpleStringProperty(tanggalKirim);
+	}
+	public SimpleStringProperty getSimpleStringPropertyTanggalCheckIn(){
+		return new SimpleStringProperty(tanggalCheckIn);
 	}
 
-	public String getAlamatPenerima() {
-		return alamatPenerima;
+	public SimpleStringProperty getSimpleStringPropertyAWB(){
+		return new SimpleStringProperty(NOAWB);
 	}
 
-	public void setAlamatPenerima(String alamatPenerima) {
-		this.alamatPenerima = alamatPenerima;
+	public SimpleStringProperty getSimpleStringPropertyPembawa(){
+		return new SimpleStringProperty(namaPembawa);
 	}
 
-	public int getIdCheckPoint() {
-		return idCheckPoint;
-	}
-
-	public void setIdCheckPoint(int idCheckPoint) {
-		this.idCheckPoint = idCheckPoint;
-	}
-
-	public int getTypeCheckPointInt() {
-		return typeCheckPointInt;
-	}
-
-	public void setTypeCheckPointInt(int typeCheckPointInt) {
-		this.typeCheckPoint= TypeCheckPointClass.getCheckPointType(typeCheckPointInt);
-		this.typeCheckPointInt = typeCheckPointInt;
-	}
-
-	public TypeCheckPoint getTypeCheckPoint() {
-		return typeCheckPoint;
-	}
-
-	public void setTypeCheckPoint(TypeCheckPoint typeCheckPoint) {
-		this.typeCheckPointInt=TypeCheckPointClass.getCheckPointInt(typeCheckPoint);
-		this.typeCheckPoint = typeCheckPoint;
-	}
-
-	public String gettypeCheckPointString(){
-		return this.typeCheckPoint.toString();
-	}
-
-	public String getNamaPembawa() {
-		return namaPembawa;
-	}
-
-	public void setNamaPembawa(String namaPembawa) {
-		this.namaPembawa = namaPembawa;
-	}
+	//
+//	public int getIdTransaction() {
+//		return idTransaction;
+//	}
+//
+//	public void setIdTransaction(int idTransaction) {
+//		this.idTransaction = idTransaction;
+//	}
+//
+//	public String getNamaPengirim() {
+//		return namaPengirim;
+//	}
+//
+//	public void setNamaPengirim(String namaPengirim) {
+//		this.namaPengirim = namaPengirim;
+//	}
+//
+//
+//	public String getNamaPenerima() {
+//		return namaPenerima;
+//	}
+//
+//	public void setNamaPenerima(String namaPenerima) {
+//		this.namaPenerima = namaPenerima;
+//	}
+//
+//	public String getAlamatPengirim() {
+//		return alamatPengirim;
+//	}
+//
+//	public void setAlamatPengirim(String alamatPengirim) {
+//		this.alamatPengirim = alamatPengirim;
+//	}
+//
+//	public String getAlamatPenerima() {
+//		return alamatPenerima;
+//	}
+//
+//	public void setAlamatPenerima(String alamatPenerima) {
+//		this.alamatPenerima = alamatPenerima;
+//	}
+//
+//	public int getIdCheckPoint() {
+//		return idCheckPoint;
+//	}
+//
+//	public void setIdCheckPoint(int idCheckPoint) {
+//		this.idCheckPoint = idCheckPoint;
+//	}
+//
+//	public int getTypeCheckPointInt() {
+//		return typeCheckPointInt;
+//	}
+//
+//	public void setTypeCheckPointInt(int typeCheckPointInt) {
+//		this.typeCheckPoint= TypeCheckPointClass.getCheckPointType(typeCheckPointInt);
+//		this.typeCheckPointInt = typeCheckPointInt;
+//	}
+//
+//	public TypeCheckPoint getTypeCheckPoint() {
+//		return typeCheckPoint;
+//	}
+//
+//	public void setTypeCheckPoint(TypeCheckPoint typeCheckPoint) {
+//		this.typeCheckPointInt=TypeCheckPointClass.getCheckPointInt(typeCheckPoint);
+//		this.typeCheckPoint = typeCheckPoint;
+//	}
+//
+//	public String gettypeCheckPointString(){
+//		return this.typeCheckPoint.toString();
+//	}
+//
+//	public String getNamaPembawa() {
+//		return namaPembawa;
+//	}
+//
+//	public void setNamaPembawa(String namaPembawa) {
+//		this.namaPembawa = namaPembawa;
+//	}
 }
